a680ba5442203457f281f1adb398530f14218c78
pavel1yakimovich <pavel1yakimovich@gmail.com>
2017-09-09 12:10:48 +0300

    Authorization with roles added

---
+++ IdentityApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using IdentityApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdentityApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (ApplicationDbContext ctx = new ApplicationDbContext())
+            {
+                IQueryable<ApplicationUser> usersWithRoles = ctx.Users.Include(u => u.Roles);
+
+                ViewData["users"] = usersWithRoles.ToList();
+                ViewData["roles"] = ctx.Roles.ToList();
+            }
+            return View();
+        }
+
+        [Authorize(Roles = "admin")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "For admin only";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
